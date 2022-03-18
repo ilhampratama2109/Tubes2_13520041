760c696d432d9c8251514f7c56bd6eab8c98ea11
rkvilena <rwad0711@gmail.com>
2022-03-18 12:35:32 +0700

    Add BFS file & DFS file (isi dasar)

---
+++ Temporary/DBFS/DFS.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+/* Data di DirectoryProcessor (barangkali perlu)
+private string startingDirectory;
+public string[] parentNode;
+public string[] childNode;
+private List<string> listOfNode = new List<string>();
+private int index;
+private Graph graph;
+private Microsoft.Msagl.Drawing.Graph visualGraph;
+private Form1 form1;
+*/
+
+
+namespace DBFS{
+    class BFS{
+        // Atribut kelas BFS
+        private List<int> path; // List untuk menyimpan data path yang dilalui
+        private DirectoryProcessor dirProcessor; // Deklarasi prosesor direktori
+        private bool[] isVisited; // array of boolean untuk menyimpan status dikunjungi sebuah node
+
+        public List<int> startSearch(Graph graph, int startnode, int targetnode){
+            isVisited = new bool[graph.CountNode()];
+            isVisited[0] = true; // Node pertama sudah dikunjungi
+            for (int i = 1; i < graph.CountNode(); i++){
+                isVisited[i] = false; // Selain node pertama diisi "belum dikunjungi"
+            }
+            path = new List<int>();
+            path.Add(startnode); // Catat path node pertama
+        }
+        private List<int> DFSrecursive(Graph graph, int startnode, int targetnode){
+
+        }
+
+
+
+    }
+}
